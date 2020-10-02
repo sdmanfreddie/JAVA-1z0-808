11103f7746510ed597d340abd7b38c9fdba16ca0
sdmanfreddie <72246235+sdmanfreddie@users.noreply.github.com>
2020-10-02 04:08:58 -0400

    Create question [ 01-03 ] .java

---
+++ question [ 01-03 ] .java
@@ -0,0 +1,15 @@
+public class Test {
+		
+  public static void main(String[] args) {
+		
+    short numPets = 5;
+	// int numGrains = 5.6;
+	String name = "Scruffy";
+
+	// numPets.length();
+	// numGrains.length();
+	name.length();
+		
+  } 
+		
+}

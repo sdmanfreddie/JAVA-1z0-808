761960cfde06dda0d0070be5a1bdae43704d4375
sdmanfreddie <72246235+sdmanfreddie@users.noreply.github.com>
2020-10-02 03:35:32 -0400

    Create question [ 01-01 ] .java

---
+++ question [ 01-01 ] .java
@@ -0,0 +1,21 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+         
+       int A$B         = 1;
+		
+       int _helloWorld = 1;
+		
+       // int true        = 1;
+		
+       int java.lang   = 1;
+		
+       int Public      = 1;
+		
+       // int 1980_s      = 1;
+        
+       System.out.println( "124" );
+        
+     }
+     
+}

7d3c610c6e23c4d1eed6196d77437a4f834dd056
sdmanfreddie <72246235+sdmanfreddie@users.noreply.github.com>
2020-10-02 03:50:52 -0400

    Create question [ 01-02 ] .java

---
+++ question [ 01-02 ] .java
@@ -0,0 +1,17 @@
+public class WaterBottle {
+		
+  private String brand;
+		
+  private boolean empty;
+		
+  public static void main(String[] args) {
+		
+    WaterBottle wb = new WaterBottle();
+    
+    System.out.print  ( "  Empty = " + wb.empty );
+    
+    System.out.println( ", Brand = " + wb.brand );
+		
+  } 
+		
+}
